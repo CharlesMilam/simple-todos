b4fe1edf87b6c870a11f34ce0cbdeb29c452426d
charles milam <charles.milam@gmail.com>
2014-12-22 16:47:03 -0600

    Modify to sort tasks by create date, descending.

--- simple-todos.js
+++ simple-todos.js
@@ -5,7 +5,9 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+      return Tasks.find({},
+      {sort: {createdAt: -1}}
+      );
     }
   });
 
