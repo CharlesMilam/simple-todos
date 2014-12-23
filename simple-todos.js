a670dc31247de4d7d4a20c9ae7d28bb48d7ac923
charles milam <charles.milam@gmail.com>
2014-12-23 17:08:52 -0600

    Add change event to body.events to set session state when hideCompleted box is checked/unchecked.

--- simple-todos.js
+++ simple-todos.js
@@ -5,13 +5,24 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({},
-      {sort: {createdAt: -1}}
-      );
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
     }
   });
 
   Template.body.events({
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
+    },
+
     "submit .new-task": function (event) {
       // function is called when the new task form is submitted
       var text = event.target.text.value;
@@ -20,6 +31,7 @@ if (Meteor.isClient) {
         text: text,
         createdAt: new Date() // current time
       });
+
       // clear the form
       event.target.text.value = "";
 
